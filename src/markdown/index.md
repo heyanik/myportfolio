01a2a9b501e721abad5a98042cee54c8961f825c
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-27 15:28:30 +0600

    setting up the markdown and style

---
+++ src/markdown/index.md
@@ -0,0 +1,15 @@
+# Anik Das Partha
+
+Currently working at [Nagram](https://www.instagram.com/nagram.com.bd/) . I like making interesting projects.
+
+Dreaming up ideas and making them come true is where my passion lies.  
+You can find my full projects list here.
+
+Outside of programming, I enjoy doing photography and traveling. Some  
+of my photos can be found on [Instagram](https://www.instagram.com/anik.me/) . If you happen to be in the same city I live  
+(currently in Bangladesh), maybe we can hang out and take some photos or code together.
+
+Find me on [Github](https://github.com/anikdaspartha58) ,[Twitter](https://twitter.com/hianikdaspartha)  
+Mail me at anikdaspartha58@gmail.com
+
+## 2022-PRESENT © Anik Das Partha
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
-
+import { useState, useEffect } from "react";
+import Markdown from "markdown-to-jsx";
 import "./HomeStyle.css";
 
 const Home = () => {
+  const [postContent, setPostcontent] = useState("");
+  useEffect(() => {
+    import("../markdown/index.md").then((res) =>
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((response) => setPostcontent(response))
+        .catch((err) => console.log(err))
+    );
+  }, []);
+
   return (
     <>
       <Header />
-      <div className="bio">
-        <h1>Anik Das Partha</h1>
-        <p>
-          Hey, I'm Anik Das Partha. Front-end developer / Amateur designer.{" "}
-          <br />
-          Currently working at{" "}
-          <a href="https://www.instagram.com/nagram.com.bd/" target="blank">
-            Nagram
-          </a>
-          . I like making interesting projects.{" "}
-        </p>
-        <p>
-          Dreaming up ideas and making them come true is where my passion lies.
-          <br />
-          You can find my <a> full projects list here</a>.
-        </p>
-        <p>
-          Outside of programming, I enjoy doing photography and traveling. Some
-          <br />
-          of my photos can be found on{" "}
-          <a href="https://www.instagram.com/anik.me/" target="blank">
-            Instagram
-          </a>
-          . If you happen to be in the same city I live
-          <br />
-          (currently in Bangladesh), maybe we can hang out and take some photos
-          or code together.
-        </p>
-        <p>
-          Find me on{" "}
-          <a href="https://github.com/anikdaspartha58" target="blank">
-            Github
-          </a>
-          ,{" "}
-          <a href="https://twitter.com/hianikdaspartha" target="blank">
-            Twitter
-          </a>
-          <br />
-          Mail me at <a>anikdaspartha58@gmail.com</a>
-        </p>
-        <h2>2022-PRESENT © Anik Das Partha</h2>
+      <div>
+        <Markdown className="markdown">{postContent}</Markdown>
       </div>
     </>
   );
